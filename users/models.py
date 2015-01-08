2b994570549d2e2a0352c13307181a9aa56ff57f
riskkim <wnsgh611@gmail.com>
2015-01-09 01:42:46 +0900

    유저 모델 에러 수정 및 유저 매니저 생

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User,UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.exceptions import ValidationError
 
 
-class User(User):
+class UsersManager(BaseUserManager):
+    def create_user(self, user_name, email=None, password=None, **kwargs):
+        self.validate_kwargs(kwargs)
+
+    def validate_kwargs(self, kwargs):
+        if not "gender" in kwargs:
+            raise ValidationError("Gender must be specified.")
+
+
+class User(AbstractBaseUser):
     MALE = 0
     FEMALE = 1
     user_email = models.EmailField(unique=True)
@@ -11,3 +21,4 @@ class User(User):
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
+    objects = UsersManager()
