3583abdf82df9458e095a85231f43d8d0ec845b1
riskkim <wnsgh611@gmail.com>
2015-01-09 02:39:32 +0900

    패스워드에 대한 모델 Validation과 데이터베이스 저장 로직 추

--- users/models.py
+++ users/models.py
@@ -6,11 +6,24 @@ from django.core.exceptions import ValidationError
 class UsersManager(BaseUserManager):
     def create_user(self, user_name, email=None, password=None, **kwargs):
         self.validate_kwargs(kwargs)
+        self.validate_password(password=password)
+        u = User()
+        u.user_email = email
+        u.user_name = user_name
+        u.set_password(raw_password=password)
+        u.gender = kwargs['gender']
+
+        u.save()
+
+        return u
 
     def validate_kwargs(self, kwargs):
         if not "gender" in kwargs:
             raise ValidationError("Gender must be specified.")
 
+    def validate_password(self, password):
+        if password is None:
+            raise ValidationError("Password must be set.")
 
 class User(AbstractBaseUser):
     MALE = 0
