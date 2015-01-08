8b7a5040d81caf5316e5264ac7427b78ef998f90
junojunho <wnsgh611@gmail.com>
2015-01-09 04:28:05 +0900

    세팅 변경 admin 축소화

--- Holynation/settings.py
+++ Holynation/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'procg_subtitle',
+    #'procg_subtitle',
     'users'
 )
 
